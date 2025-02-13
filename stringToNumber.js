6685c8939ac15378aecbca3ccaba9c8f643da550
hrithikD9 <hrithikdevnath0924@gmail.com>
2025-02-13 15:10:02 +0600

    New file added

---
+++ stringToNumber.js
@@ -0,0 +1,20 @@
+let str1 = "Hello"
+let str2 = " Hrithik"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 +  2 + "2"); // 32
+
+
+let gamecounter = 99;
+gamecounter++; // post increment
+console.log(gamecounter); // 100
+--gamecounter; // pre increment
+console.log(gamecounter); // 99
+
+
+
+
