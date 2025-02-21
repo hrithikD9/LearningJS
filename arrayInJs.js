70209771198d454d05b60199163e795de0be306e
hrithikD9 <hrithikdevnath0924@gmail.com>
2025-02-21 12:27:20 +0600

    edited

--- arrayInJs.js
+++ arrayInJs.js
@@ -1,7 +1,7 @@
 // Array Declaration
 
 const myArr = [0,1,2,3,4,5]
-console.log(myArr[2]);
+console.log(myArr[2]); // 2
 
 const myHeroes = ["shaktiman","Superman"]
 
@@ -14,17 +14,17 @@ myArr.pop()
 
 myArr.unshift(9)
 myArr.shift()
-console.log(myArr);
+console.log(myArr); // [ 0, 1, 2, 3, 4, 5 ]
 
-console.log(myArr.includes(9));
-console.log(myArr.indexOf(9));
+console.log(myArr.includes(9)); // false
+console.log(myArr.indexOf(9)); // -1
 
 const newArrr = myArr.join();
 
-console.log(myArr);
-console.log(typeof myArr);
-console.log(newArrr);
-console.log(typeof newArrr);
+console.log(myArr); // [ 0, 1, 2, 3, 4, 5 ]
+console.log(typeof myArr); // object
+console.log(newArrr); // 0,1,2,3,4,5
+console.log(typeof newArrr); // string
 
 
 console.log("A",myArr); // A [ 0, 1, 2, 3, 4, 5 ]
@@ -36,5 +36,3 @@ console.log("B",myArr); // B [ 0, 1, 2, 3, 4, 5 ]
 const myn2 = myArr.splice(1,3)
 console.log(myn2); // [ 1, 2, 3 ]
 console.log("C",myArr); // C [ 0, 4, 5 ]
-
-
