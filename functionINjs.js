ac704cae25166a650eff62dbf5ca4c189472126b
hrithikD9 <hrithikdevnath0924@gmail.com>
2025-02-23 14:40:57 +0600

    first commit

---
+++ functionINjs.js
@@ -0,0 +1,49 @@
+function addTwoNumbers(number1 , number2){
+    console.log(number1 + number2);
+    return number1 + number2;
+}
+
+const x = addTwoNumbers(2 , 5)
+
+console.log("Result : ", x);
+
+function loginUserMessage(userName){
+    if(userName === undefined){
+        console.log(" Enter the name ");
+        return
+    }
+    return `${userName} just logged in`
+}
+
+console.log(loginUserMessage("Hrithik"));
+
+
+function calculateCartPrice(...num1){ // (...) these are rest operator
+    return num1
+}
+
+console.log(calculateCartPrice(100,200,300));
+
+const user = {
+    name : "abcd",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`The userName is ${anyobject.name} , and the price is ${anyobject.price}`);
+}
+
+handleObject(user)
+
+handleObject({   // another method of passing values  in obejct
+    name : "hhhh",
+    price : 99
+})
+
+const myNewArray = [200,300,400]
+
+function returnSecondValue(getArray){  // method of passing values  in Array
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
