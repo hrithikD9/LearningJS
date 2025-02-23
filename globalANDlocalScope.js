91264b63927b96806df853100eb68b980fd23b5d
hrithikD9 <hrithikdevnath0924@gmail.com>
2025-02-23 20:06:33 +0600

    first commit

---
+++ globalANDlocalScope.js
@@ -0,0 +1,32 @@
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "Hrithik"
+
+    function two(){
+        const website = "instagram"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two();
+}
+// one()
+
+function addOne(num){
+    return num + 1;
+}
+console.log(addOne(7));
+
+const addTwo =  function (num){ // another method of declaration function
+    return num + 2;
+}
+console.log(addTwo(7));
